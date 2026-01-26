12d2b1e817b750967100eb4bc17dbb3c6a79d2d0
oMarcosLeite <marcosale.leite@gmail.com>
2026-01-26 09:37:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown("---")
 st.title("🏡 Portal Cacau Imob")
 
 # Coloque aqui o nome da sua foto que está no GitHub
-st.image("foto2.jpg", use_container_width=True)
+st.image("foto2.jpeg", use_container_width=True)
 
 st.write("Bem-vindo ao nosso refúgio exclusivo. Explore nossa curadoria de imóveis.")
 
